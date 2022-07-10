d3d20d8938e435f2333a716c405a4b057b164335
Jayden Callan <jqcallan04@gmail.com>
2022-07-09 18:13:08 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hi. Enter your name below:");
+            var teacher_name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {teacher_name}! How are you today?");
+        }
+    }
+}
